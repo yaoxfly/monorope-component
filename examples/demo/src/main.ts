b2101537ad36b8d50f5c12ad0cad32165c6ad9f7
yaoxfly <1315892088@qq.com>
2023-09-17 17:44:31 +0800

    feat：组件demo优化

--- examples/demo/src/main.ts
+++ examples/demo/src/main.ts
@@ -3,7 +3,8 @@ import router from './router'
 import { createPinia } from 'pinia'
 import '@/assets/css/index.css'
 import ElementPlus from 'element-plus'
-import ComponentPc from 'component-pc'
+// import ComponentPc from 'component-pc'
+import ComponentPc from '../../../packages/component-pc/index'
 createApp(App)
   .use(router)
   .use(createPinia())
