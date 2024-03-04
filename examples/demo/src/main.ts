b8825643f2d1fc58b9ea3f94d24168fa3280cc70
yaoxfly <1315892088@qq.com>
2024-03-04 10:03:17 +0800

    test:组件测试

--- examples/demo/src/main.ts
+++ examples/demo/src/main.ts
@@ -2,13 +2,13 @@ import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
 import '@/assets/css/index.css'
-import ElementPlus, { ElButton } from 'element-plus'
-import ComponentPc, { Button } from 'component-pc'
-console.log(Button, ElButton)
+import ElementPlus from 'element-plus'
+import ComponentPc from 'component-pc'
+// console.log(Test)
 // import ComponentPc from '../../../packages/component-pc/index'
 createApp(App)
   .use(router)
   .use(createPinia())
   .use(ElementPlus)
-  .use(Button)
+  .use(ComponentPc)
   .mount('#app')
--- examples/demo/src/views/home/index.tsx
+++ examples/demo/src/views/home/index.tsx
@@ -1,11 +1,14 @@
 import config from '@/config'
+// import { Button } from 'component-pc'
 export default defineComponent({
   name: 'Home',
+
   setup () {
     // 主题切换
     const setTheme = () => {
       document.documentElement.style.setProperty('--color-primary-base', '16,185,129')
     }
+
     setTheme()
     const input = useStorage('input-value', '可同步数据到localstorage的输入框') // vueuse的api，自动将ref同步到localstorage,实现双向绑定
     return () => {
@@ -13,7 +16,8 @@ export default defineComponent({
       console.log(route.query, '传递给home页面的参数')
       return (
         <>
-          <yx-button></yx-button>
+
+          <yx-button ></yx-button>
           <div class='tw-flex tw-justify-center  tw-items-center  tw-bg-gray-50  tw-text-black'>
               <span > 我是首页{config.server}</span>
           </div>
